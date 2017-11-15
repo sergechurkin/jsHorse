fadb0ea51c69ad580da7156147202d688cc1319c
sergechurkin <sergechurkin@yandex.ru>
2017-11-15 18:40:07 +0300

    commit 1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # jsHorse
 3D animation on js based on the library of three.js and MD2 models.
+
+![Скрин приложения](jsHorse.png)
+
+## Description
+
+The application is a 3D animation based on the library [three.js](https://github.com/mrdoob/three.js).
+It uses [MD2 models of horse and bird](http://telias.free.fr/models_md2_menu.html),
+converted to json format by [OutsideOfSociety service](http://oos.moxiecode.com/js_webgl/md2_converter/).
+
+By mouse clicking the horse turns into a bird and back.
+
+By clicking on `F1` or mouse right clicking you can adjust the number of animated objects. By the `Esc` key, you can stop the animation, by `Enter` - start.
+
+[Run application.]( http://sergechurkin.vacau.com/horse.html)
+
+3D анимация на js на базе библиотеки three.js и MD2 моделей.
+
+## Описание
+
+Приложение представляет собой 3D анимацию на базе библиотеки [three.js](https://github.com/mrdoob/three.js). 
+В нем использованы [MD2 модели лошади и птицы](http://telias.free.fr/models_md2_menu.html),
+Преобразованный в формат json [сервисом OutsideOfSociety](http://oos.moxiecode.com/js_webgl/md2_converter/). 
+
+По клику мыши лошадь превращается в птицу и обратно. 
+
+По нажатию на `F1` или клику на правую кнопку можно настроить количество анимируемых объектов. По кнопке `Esc` можно остановить анимацию, по `Enter` – запустить.
+
+[Запустить приложение.]( http://sergechurkin.vacau.com/horse.html)
---
+++ js/tags_forms.js
@@ -0,0 +1,54 @@
+﻿function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function tag_h(size, text, color) {
+  return '<h' + size + ' align="center" style="color: ' + color + '">' + text + '</h' + size + '>';
+}
+
+function tag_button(text, script) {
+  return '<button style="border-radius: 2px" id="' + text + '" onclick="' + script + '">' + text + '</button>';
+}
+
+function tag_form(legend, form, buttons) {
+  var str = '';
+  var strButtons = '';
+  for (var i = 0; i < form.length; i++) {
+    str = str + '<label for="' + form[i].name + '">' + form[i].label + ': <' + form[i].tag + 
+      ' name="' + form[i].name + '" id="' + form[i].name + '"';
+    if (form[i].tag === 'input') {
+      str = str + ' type="' + form[i].type + '" class="' + form[i].tag + '" value="' + form[i].value + 
+        '" onblur="numValidate(this);" onfocus="clearMess(this);">' + 
+        ' <a id="error' + form[i].name + '" class="errormess"></a>';
+    }
+    if (form[i].tag === 'select') {
+      str = str + '>';
+      for (var j = 0; j < form[i].options.length; j++) {
+        str = str + '<option ' + (form[i].options[j].selected ? 'selected' : '') + '>' + form[i].options[j].value + '</option>';
+      }
+      str = str + '</select>';
+    }
+    str = str + '</label><br />';
+  }
+  strButtons = strButtons + '<div style="margin: 5px" align="center">';
+  for (var i = 0; i < buttons.length; i++) {
+    strButtons = strButtons + tag_button(buttons[i].label, buttons[i].script);
+  }
+  strButtons = strButtons + '</div>';
+  return '<fieldset><legend>' + legend + '</legend>' + str + '</fieldset>' + strButtons;
+}
+
+function numValidate(e) {
+  if (isNaN(e.value) || Number(e.value) < 1) {
+    e.className = "error";
+    document.getElementById('error' + e.id).innerHTML = 'Enter a positive number';
+  }
+}
+
+function clearMess(e) {
+  e.className = "input";
+    document.getElementById('error' + e.id).innerHTML = ''
+}
